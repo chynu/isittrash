4bd00e823d4415225c570ae8bfe3d47bbb0fbbaf
cel <celine.choo98@gmail.com>
2016-09-17 08:22:07 -0400

    added backend frame

---
+++ main.py
@@ -0,0 +1,30 @@
+import webapp2
+import os
+import jinja2
+import random
+import logging
+
+jinja_environment = jinja2.Environment(loader =
+    jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+# ================== HANDLERS ====================
+""" HANDLER INFORMATION
+    url: /
+    handler: IndexHandler
+    frontend: /templates/index.html,            /stylesheets/index.css     """
+class IndexHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('templates/index_v2.html')
+
+        temp = {
+            "username": nick,
+            "log_url": log,
+            "log_text": log_text,
+            "dash_text": dash_text
+        }
+        self.response.write(template.render(temp))
+
+# ============== LINKS ===============
+app = webapp2.WSGIApplication([
+    ('/', IndexHandler)
+], debug=True)
