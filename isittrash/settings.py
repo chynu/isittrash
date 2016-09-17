3d189759d0239211b9d49135b57a1f22bc679621
cel <celine.choo98@gmail.com>
2016-09-17 17:27:41 -0400

    more backend changes

--- isittrash/settings.py
+++ isittrash/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -77,7 +76,7 @@ WSGI_APPLICATION = 'isittrash.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'rango.db'),
     }
 }
 
